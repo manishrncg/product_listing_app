ea393757d0297bdc03cb50668ecb3a5ba39bc1df
Manish <manishrncg@gmail.com>
2018-11-11 01:12:54 +0530

    dependencies added(axios, react-router-dom) & data fetching from mock server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import {apiEndPoint} from './config/config.js';
 import './App.css';
 
+import { Link } from 'react-router-dom';
+
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = ({
+			cars: []
+		});
+	}
+	componentDidMount(){
+		const self = this;
+		axios.get(apiEndPoint+'/cars')
+		.then(function (response) {
+			console.log(response);
+			self.setState({
+				cars: response.data.cars
+			});
+		})
+		
+	}
+
   render() {
+  	const cars = this.state.cars.map( (car,index) => {
+  		const carName = car.manufacturerName + ' ' + car.modelName;
+  		return (<div key={index} className="product-box display-flex">
+					<img src={car.pictureUrl} className="car-dp" alt={carName} />
+					<span className="product-desc">
+						<b className="margin-0">{carName}</b>
+						<p className="desc">Stock # {car.stockNumber} - {car.mileage.number + car.mileage.unit} - {car.fuelType} - {car.color}</p>
+							<Link to={'/view/'+car.stockNumber} className="cursor-pointer orange-text">View details</Link>
+					</span>
+				</div>);
+  	});
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+		<div className="display-flex">
+			<div className="grey-border-box padding-15 flex1">
+				
+			</div>
+			<div className='product-list'>
+				<div className="display-flex">
+					<div className="flex1">
+						<h2 className="margin-tb-15">Available cars</h2>
+						<h2 className="font-w-normal margin-tb-15">Showing 10 of 100 results</h2>
+					</div>
+					<div className="flex1">
+						
+					</div>
+				</div>
+				{cars}
+				<div className="width-pagination margin-auto">
+					
+				</div>
+			</div>
+		</div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import react router deps
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+ReactDOM.render(
+	<React.Fragment>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={App} />
+			</Switch>
+		</BrowserRouter>
+	</React.Fragment>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
